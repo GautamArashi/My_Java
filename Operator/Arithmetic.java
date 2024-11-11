c078a728299a56b458110bd61ad7b2e7262b3edf
GautamArashi <gautamarashi8877@gmail.com>
2024-11-11 22:04:04 +0530

    Arithmetic operator

---
+++ Operator/Arithmetic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+/* Used for basic mathematical operations.
+Operators: + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus). */
+public class Arithmetic {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Add:= " + (a + b));
+        System.out.println("Subtract: " + (a - b));
+        System.out.println("Multiply: " + (a * b));
+        System.out.println("Divide: " + (a / b));
+        System.out.println("Modulo: " + (a % b));
+    }
+}
---
+++ Operator/Operator.js
@@ -0,0 +1,4 @@
+/*
+What  is operators?
+= an operator is a symbol that performs a specific operation on one, two, or more operands, producing a result
+*/
